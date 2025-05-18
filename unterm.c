5f8649bd80f264efbcf0d675429533d82b31106c
chen <yurenchen@yeah.net>
2025-05-18 22:44:34 +0800

    U unterm.c: adjust unterm.c for vterm_strip purpose

--- unterm.c
+++ unterm.c
@@ -1,3 +1,7 @@
+// this tool copied from libvterm, and adapted for vterm_strip
+//  https://www.leonerd.org.uk/code/libvterm/
+//  depends libvterm v0.3.3
+
 #include <stdio.h>
 #include <string.h>
 
@@ -8,7 +12,47 @@
 
 #include "vterm.h"
 
-#include "../src/utf8.h" // fill_utf8
+// #include "../src/utf8.h" // fill_utf8
+/* The following functions copied and adapted from libtermkey
+ *
+ * http://www.leonerd.org.uk/code/libtermkey/
+ */
+static inline unsigned int utf8_seqlen(long codepoint)
+{
+  if(codepoint < 0x0000080) return 1;
+  if(codepoint < 0x0000800) return 2;
+  if(codepoint < 0x0010000) return 3;
+  if(codepoint < 0x0200000) return 4;
+  if(codepoint < 0x4000000) return 5;
+  // return 6;
+  return 0;
+}
+
+/* Does NOT NUL-terminate the buffer */
+static int fill_utf8(long codepoint, char *str)
+{
+  int nbytes = utf8_seqlen(codepoint);
+
+  // This is easier done backwards
+  int b = nbytes;
+  while(b > 1) {
+    b--;
+    str[b] = 0x80 | (codepoint & 0x3f);
+    codepoint >>= 6;
+  }
+
+  switch(nbytes) {
+    case 1: str[0] =        (codepoint & 0x7f); break;
+    case 2: str[0] = 0xc0 | (codepoint & 0x1f); break;
+    case 3: str[0] = 0xe0 | (codepoint & 0x0f); break;
+    case 4: str[0] = 0xf0 | (codepoint & 0x07); break;
+    case 5: str[0] = 0xf8 | (codepoint & 0x03); break;
+    case 6: str[0] = 0xfc | (codepoint & 0x01); break;
+  }
+
+  return nbytes;
+}
+/* end copy */
 
 /*
  * unterm [OPTIONS] SCRIPTFILE
@@ -108,10 +152,10 @@ static void dump_cell(const VTermScreenCell *cell, const VTermScreenCell *prevce
         if(prevcell->attrs.reverse && !cell->attrs.reverse)
           sgr[sgri++] = 27;
 
-        if(!prevcell->attrs.conceal && cell->attrs.conceal)
-          sgr[sgri++] = 8;
-        if(prevcell->attrs.conceal && !cell->attrs.conceal)
-          sgr[sgri++] = 28;
+        // if(!prevcell->attrs.conceal && cell->attrs.conceal)
+        //   sgr[sgri++] = 8;
+        // if(prevcell->attrs.conceal && !cell->attrs.conceal)
+        //   sgr[sgri++] = 28;
 
         if(!prevcell->attrs.strike && cell->attrs.strike)
           sgr[sgri++] = 9;
@@ -166,7 +210,8 @@ static void dump_eol(const VTermScreenCell *prevcell)
     case FORMAT_SGR:
       if(prevcell->attrs.bold || prevcell->attrs.underline || prevcell->attrs.italic ||
          prevcell->attrs.blink || prevcell->attrs.reverse || prevcell->attrs.strike ||
-         prevcell->attrs.conceal || prevcell->attrs.font)
+        //  prevcell->attrs.conceal || prevcell->attrs.font)
+         prevcell->attrs.font)
         printf("\x1b[m");
       break;
   }
